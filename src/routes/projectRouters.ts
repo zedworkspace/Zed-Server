ad2f719a51e6d6166320ad046d83baa0d59723f4
zedworkspace <shanoof.001@gmail.com>
2025-02-19 10:24:58 +0530

    feat(project):create project api

--- src/routes/projectRouters.ts
+++ src/routes/projectRouters.ts
@@ -1,5 +1,9 @@
 import express, { Router } from "express";
+import { createProject } from "../controllers/projectController";
+import upload from "../middlewares/imageUploadingMiddleware";
 
 const projectRouter: Router = express.Router();
+
+projectRouter.route("/").post(upload.single("projectLogo"), createProject);
 
 export default projectRouter;
--- src/routes/projectRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createProject } from "../controllers/projectController";
-import upload from "../middlewares/imageUploadingMiddleware";
-
-const projectRouter = express.Router();
-
-projectRouter.route("/").post(upload.single("projectLogo"), createProject);
-
-export default projectRouter;
