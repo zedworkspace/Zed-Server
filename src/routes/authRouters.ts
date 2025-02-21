21a469078443de782fb425f53f455200528583b0
AbhayPc <abhaypc112@gmail.com>
2025-02-21 12:20:56 +0530

    Add Reset Password & Handle Build Errors

--- src/routes/authRouters.ts
+++ src/routes/authRouters.ts
@@ -1,5 +1,5 @@
 import express, { Router } from 'express';
-import { accessTokenGenerator, emailRegister, emailSignIn, sendOtp } from '../controllers/authControllers';
+import { accessTokenGenerator, emailRegister, emailSignIn, resetPassword, sendOtp, sendResetOtp } from '../controllers/authControllers';
 import { userAuth } from '../middlewares/userAuth';
 
 const authRouter :Router = express.Router();
@@ -8,5 +8,7 @@ authRouter.post('/v1/otp-request',sendOtp);
 authRouter.post('/v1/register',emailRegister);
 authRouter.post('/v1/signin',emailSignIn);
 authRouter.post('/v1/get-access-token',accessTokenGenerator);
+authRouter.post('/v1/reset-otp-request',sendResetOtp);
+authRouter.post('/v1/reset-password',resetPassword);
 
 export default authRouter;
