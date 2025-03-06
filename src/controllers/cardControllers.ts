7266a958876207a73f102f9a3936ac34737cad7b
nishananaaah <nishanaf731@gmail.com>
2025-03-06 17:03:38 +0530

    feat(card):create upodate card api

--- src/controllers/cardControllers.ts
+++ src/controllers/cardControllers.ts
@@ -22,3 +22,24 @@ export const getCardById = catchAsync(async (req, res) => {
     data: card,
   });
 });
+
+export const editCardById = catchAsync(async (req, res) => {
+  const { cardId } = req.params;
+
+  const updateData = {
+    title: req.body.title,
+    labels: req.body.labels,
+    assignees: req.body.assignees,
+    description:req.body.description,
+  };
+  const updatedCard = await cardServices.editCardById(cardId, updateData);
+  if (!updatedCard) {
+    return res.status(404).json({ message: "Card not found" });
+  }
+
+  res.status(200).json({
+    status: "Success",
+    message: "Card updated successfully",
+    data: updatedCard,
+  });
+});
