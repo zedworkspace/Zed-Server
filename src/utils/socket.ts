afe4a65b2837407b1b02517c77a0a093451ce0f9
zedworkspace <shanoof.001@gmail.com>
2025-03-17 00:17:27 +0530

    fix(board):fix onDrop Card position to last

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -109,9 +109,13 @@ export const initializeSocket = (server: any) => {
     });
 
     socket.on("onCardDrop", async (fromListId, cardId, toListId, boardId) => {
+      const lastCard = await Card.findOne({ listId: toListId }).sort(
+        "-position"
+      );
+      const newPosition = lastCard && lastCard?.position + 1;
       await Card.findOneAndUpdate(
         { _id: cardId },
-        { $set: { listId: toListId } }
+        { $set: { listId: toListId, position: newPosition } }
       );
       handleUpdatedBoard({ io, boardId });
     });
