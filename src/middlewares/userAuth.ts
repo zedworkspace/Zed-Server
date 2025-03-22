d3ef1e875c07c8d5c3a5a41b15bc3a8b34854e1a
MohammedJasim001 <jasimat001@gmail.com>
2025-03-22 11:40:08 +0530

    check:latest changes

--- src/middlewares/userAuth.ts
+++ src/middlewares/userAuth.ts
@@ -5,21 +5,27 @@ import { config } from "../configs/config";
 import User from "../models/userModel";
 import { IUser } from "../interfaces/userInterface";
 
-export const userAuth = async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers["authorization"]?.split(" ")[1];
 
-    if (!token) return next(new CustomError("Access denied, token missing!", 401));
+export const userAuth = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers["authorization"]?.split(" ")[1];
 
-    const JWT_SECRET_KEY = config.JWT_SECRET_KEY;
-    if (!JWT_SECRET_KEY) return next(new CustomError("Key missing!", 400));
+  if (!token)
+    return next(new CustomError("Access denied, token missing!", 401));
 
-    try {
-        const decoded = jwt.verify(token, JWT_SECRET_KEY) as JwtPayload;
-        const user = await User.findById(decoded.userId);
-        if (!user) return next(new CustomError("Access Forbidden", 403));
-        req.user = user as IUser;
-        next();
-    } catch (error) {
-        next(new CustomError("Invalid token", 401));
-    }
+  const JWT_SECRET_KEY = config.JWT_SECRET_KEY;
+  if (!JWT_SECRET_KEY) return next(new CustomError("Key missing!", 400));
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET_KEY) as JwtPayload;
+    const user = await User.findById(decoded.userId);
+    if (!user) return next(new CustomError("Access Forbidden", 403));
+    req.user = user as IUser;
+    next();
+  } catch (error) {
+    next(new CustomError("Invalid token", 401));
+  }
 };
